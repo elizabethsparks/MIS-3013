49cc027c5743f1e96350d8c60d069aec300a8ac6
Elizabeth Sparks <Elizabethsparks@ou.edu>
2020-01-22 12:54:09 -0600

    Created variables for firstName, lastName, and favoriteNumber. Also derived crazyNumber from it.

--- InClassExamples/Variables/Program.cs
+++ InClassExamples/Variables/Program.cs
@@ -11,6 +11,28 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            //Create a string named firstName
+            string firstName;
+
+            //Assign a value to firstName (Elizabeth Sparks)
+            firstName = "Elizabeth";
+
+            //Create an integer named favoriteNumber and assign your favorite number to it
+            int favoriteNumber = 13;
+
+            double crazyNumber = 7.6767 * favoriteNumber;
+
+            string lastName = "Sparks";
+            string fullName = firstName + " " + lastName;
+
+
+            Console.WriteLine(fullName + " has the favorite number of " + favoriteNumber 
+                + " \nwhich equates to a crazy number of " + crazyNumber);
+
+            // MAKE IT DYNAMIC
+
+
+            Console.ReadKey();
         }
     }
 }
