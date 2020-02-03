fef654d6550af780bdc72a8e4bd7b84c64926982
Elizabeth Sparks <Elizabethsparks@ou.edu>
2020-02-03 13:05:37 -0600

    Loops (For and Do)

---
+++ Loop/Loop/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what number do you want to count up to? >>");
+            int threshold = Convert.ToInt32(Console.ReadLine());
+
+
+            //could also use i < 101
+            for (int i = 0; i <+ threshold; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            double sum = 0; // this is an accumulator variable
+            string answer;
+
+            do
+            {
+                Console.WriteLine("Please give us a # to add >>");
+                int number = Convert.ToInt32(Console.ReadLine());
+                //sum = sum + number; 
+                sum += number;
+
+                Console.WriteLine("Do you have another # to add? (Y/N) >>");
+                answer = Console.ReadLine();
+
+
+            } while (answer == "Y");
+            //} while (answer != "N");
+            Console.WriteLine($"Your summation of values is {sum.ToString("N2")}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Participations/CoinToss/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinToss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            //use the next method on 'rand' and pass in 0,2 to generate a random number that is 0 or 1
+            int randomNumber = 0;
+            int usersGuess = 0;
+
+            while (usersGuess != randomNumber)
+            {
+
+                randomNumber = rand.Next(0, 2);
+                Console.WriteLine(randomNumber);
+
+
+                Console.WriteLine("Enter Heads or Tails >>");
+                string usersGuessAsString = Console.ReadLine();
+                
+                const int HEADS_Value = 0;
+                const int TAILS_VALUE = 1;
+
+
+                if (usersGuessAsString == "Heads");
+                else if (usersGuessAsString == "Tails");
+                else; 
+                
+
+            }
+
+
+
+
+             Console.ReadKey();
+        }
+
+}
+   
+    } 
